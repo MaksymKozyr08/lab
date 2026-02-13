181304f35364f0e07f121db62fb6b8afdf85f17a
Maksym Kozyr <maxkozyr2008@gmail.com>
2026-02-13 14:59:33 +0200

    add

--- 2/2a.cpp
+++ 2/2a.cpp
@@ -1,12 +1,24 @@
 #include <iostream>
-#include <vector>
-#include <stack>
-
+#include <queue>
 using namespace std;
 
 typedef long double ld;
 typedef long long ll;
+void print(queue<ll>& a){
+    while(!a.empty()){
+        cout<<a.back()<<" ";
+    }
+    cout<<"\n";
+}
 
 int main(){
+    ll n;
+    cin>>n;
+    queue<ll> a;
+    for(ll i=0;i<n;++i){
+        a.push(rand()%10);
+    }
+    print(a);
+    
     return 0;
 }
