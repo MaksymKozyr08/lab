9eb11f374a86d9645b4b35ff38389c365da3a778
Maksym Kozyr <maxkozyr2008@gmail.com>
2026-02-13 17:05:25 +0200

    add

--- 2/2a.cpp
+++ 2/2a.cpp
@@ -1,16 +1,67 @@
 #include <iostream>
 #include <queue>
+#include <vector>
 using namespace std;
 
 typedef long double ld;
 typedef long long ll;
 
-struct Rectangle{
+//--------------------------------------------------------------
+// struct
+struct rectangle{
     ll leftlowerx;
     ll leftlowery;
     ll weight;
     ll hight;
 };
+
+struct staticmasive{
+    rectangle date[100];
+    ll head;
+    ll tail;
+    ll count;
+};
+//--------------------------------------------------------------
+
+//--------------------------------------------------------------
+// create
+void createstatilmasive(staticmasive& a){
+    a.head=0;
+    a.tail=0;
+    a.count=0;
+    for(ll i=0;i<100;++i){
+        a.date[i].leftlowerx=0;
+        a.date[i].leftlowery=0;
+        a.date[i].weight=0;
+        a.date[i].hight=0;
+    }
+}
+//--------------------------------------------------------------
+
+
+//--------------------------------------------------------------
+//add
+void add_element(staticmasive& a, rectangle val){
+    if(a.count==100)return;
+    a.date[a.tail]=val;
+    a.tail=(a.tail+1)%100;
+    a.count++;
+}
+//--------------------------------------------------------------
+
+
+
+//--------------------------------------------------------------
+//delete
+void delete_element(staticmasive& a){
+    if(a.count==0)return;
+    a.head=(a.head+1)%100;
+    a.count--;
+}
+//--------------------------------------------------------------
+
+//--------------------------------------------------------------
+//start program
 ll startprogram(){
     cout<<"Select a number from the list for further action";
     cout<<"1:delete element ";
@@ -20,15 +71,28 @@ ll startprogram(){
     cin>>n;
     return n;
 }
+//--------------------------------------------------------------
 
-void print(queue<ll>& a){
-    while(!a.empty()){
-        cout<<a.back()<<" ";
-    }
+//--------------------------------------------------------------
+//print
+void print1(vector<ll>& a){
+    for(ll i=0;i<a.size();++i)cout<<a[i]<<" ";
     cout<<"\n";
 }
 
+void print(queue<ll> a){
+    while(!a.empty()){
+        cout<<a.front()<<" ";
+        a.pop();
+    }
+    cout<<"\n";
+}
+//--------------------------------------------------------------
+
 int main(){
+    staticmasive myStaticQ;
+    createstatilmasive(myStaticQ);
+
     ll n;
     cin>>n;
     queue<ll> a;
