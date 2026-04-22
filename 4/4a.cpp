cb7fb3070d2cd533e2b12a24001b9f74c1f9eea9
Maksym Kozyr <maxkozyr2008@gmail.com>
2026-04-22 13:51:16 +0300

    fix some problem

--- 4/4a.cpp
+++ 4/4a.cpp
@@ -1,144 +1,306 @@
 #include <iostream>
 #include <vector>
+#include <string>
+#include <queue>
+#include <cstdlib>
+#include <ctime>
+
 using namespace std;
-typedef long double ld;
+
 typedef long long ll;
 
-class TreeNode{
+class TreeNode {
 public:
-    ll date;
+    ll data;
     vector<TreeNode*> children;
-    TreeNode(ll value):date(value){}
+    TreeNode(ll val) : data(val) {}
 };
 
-class NaryTree{
+class NaryTree {
 private:
     TreeNode* root;
-    TreeNode* get_node_by_path(TreeNode* curr,const vector<ll>& p,ll idx){
-        if(!curr)return nullptr;
-        if(idx==p.size())return curr;
-        if(p[idx]<0||p[idx]>=curr->children.size())return nullptr;
-        return get_node_by_path(curr->children[p[idx]],p,idx+1);
+
+    TreeNode* get_node_by_path(TreeNode* curr, const vector<ll>& path, ll idx) {
+        if (!curr) return nullptr;
+        if (idx == path.size()) return curr;
+        if (path[idx] < 0 || path[idx] >= curr->children.size()) return nullptr;
+        return get_node_by_path(curr->children[path[idx]], path, idx + 1);
     }
-    void print_recursive(TreeNode* node,ll level){
-        if(!node)return;
-        for(ll i=0;i<level;i++)cout<<"  ";
-        cout<<"- "<<node->date<<"\n";
-        for(ll i=0;i<node->children.size();i++){
-            print_recursive(node->children[i],level+1);
+
+    bool find_path_helper(TreeNode* node, ll val, vector<ll>& curPath, vector<ll>& resPath) {
+        if (!node) return false;
+        if (node->data == val) {
+            resPath = curPath;
+            return true;
+        }
+        for (size_t i = 0; i < node->children.size(); i++) {
+            curPath.push_back(i);
+            if (find_path_helper(node->children[i], val, curPath, resPath)) return true;
+            curPath.pop_back();
+        }
+        return false;
+    }
+
+    void print_recursive(TreeNode* node, ll level) {
+        if (!node) return;
+        for (ll i = 0; i < level; i++) cout << "  ";
+        cout << "- " << node->data << "\n";
+        for (size_t i = 0; i < node->children.size(); i++) {
+            print_recursive(node->children[i], level + 1);
         }
     }
+
+    void add_prob_helper(TreeNode* curr, ll val, double p) {
+        if (!curr) return;
+        double r = (double)rand() / RAND_MAX;
+        if (r <= p || curr->children.empty()) {
+            curr->children.push_back(new TreeNode(val));
+        } else {
+            ll random_child = rand() % curr->children.size();
+            add_prob_helper(curr->children[random_child], val, p);
+        }
+    }
+
 public:
-    NaryTree():root(nullptr){}
-    void add_node(const vector<ll>& path,ll val){
-        TreeNode* temp=new TreeNode(val);
-        if(!root){
-            root=temp;
+    NaryTree() { root = nullptr; }
+
+    void add_node(const vector<ll>& path, ll val) {
+        TreeNode* tmp = new TreeNode(val);
+        if (!root) {
+            root = tmp;
             return;
         }
-        TreeNode* parent=get_node_by_path(root,path,0);
-        if(parent){
-            parent->children.push_back(temp);
-        }else{
-            cout<<"Invalid path!\n";
-            delete temp;
+        TreeNode* parent = get_node_by_path(root, path, 0);
+        if (parent) {
+            parent->children.push_back(tmp);
+        } else {
+            cout << "Path not found!\n";
+            delete tmp;
         }
     }
-    TreeNode* delete_node(const vector<ll>& path){
-        if(!root)return nullptr;
-        if(path.empty()){
-            TreeNode* temp=root;
-            root=nullptr;
-            return temp;
+
+    void add_with_probability(ll val, double p) {
+        if (!root) {
+            root = new TreeNode(val);
+        } else {
+            add_prob_helper(root, val, p);
         }
-        vector<ll> parent_path=path;
-        ll child_idx=parent_path.back();
-        parent_path.pop_back();
-        TreeNode* parent=get_node_by_path(root,parent_path,0);
-        if(parent&&child_idx>=0&&child_idx<parent->children.size()){
-            TreeNode* target=parent->children[child_idx];
-            parent->children.erase(parent->children.begin()+child_idx);
+    }
+
+    bool get_path_to_element(ll val, vector<ll>& outPath) {
+        vector<ll> tempPath;
+        return find_path_helper(root, val, tempPath, outPath);
+    }
+
+    TreeNode* delete_node(const vector<ll>& path) {
+        if (!root) return nullptr;
+        if (path.empty()) {
+            TreeNode* tmp = root;
+            root = nullptr;
+            return tmp;
+        }
+        vector<ll> pPath = path;
+        ll childIdx = pPath.back();
+        pPath.pop_back();
+
+        TreeNode* parent = get_node_by_path(root, pPath, 0);
+        if (parent && childIdx >= 0 && childIdx < parent->children.size()) {
+            TreeNode* target = parent->children[childIdx];
+            parent->children.erase(parent->children.begin() + childIdx);
             return target;
         }
-        cout<<"Invalid path for deletion!\n";
         return nullptr;
     }
-    void print_tree(){
-        if(!root){cout<<"Empty\n";return;}
-        print_recursive(root,0);
+
+    TreeNode* delete_by_value(ll val) {
+        vector<ll> path;
+        if (get_path_to_element(val, path)) {
+            return delete_node(path);
+        }
+        return nullptr;
     }
-    void demo(){
-        cout<<"--- DEMO MODE ---\n";
-        add_node({},10);
-        add_node({},20);
-        add_node({},30);
-        add_node({0},40);
-        add_node({0},50);
-        add_node({1},60);
-        cout<<"Tree Structure:\n";
-        print_tree();
-        cout<<"\nDeleting subtree at path [0, 1] (Node 50)...\n";
-        TreeNode* deleted=delete_node({0,1});
-        if(deleted)cout<<"Deleted node val: "<<deleted->date<<"\n";
-        cout<<"\nTree after deletion:\n";
-        print_tree();
-        cout<<"\n";
+
+    void clear_memory(TreeNode* node) {
+        if (!node) return;
+        for (auto child : node->children) {
+            clear_memory(child);
+        }
+        delete node;
     }
-    void print_task_tree(){
-        cout<<"--- TASK TREE (BONUS) ---\n";
-        cout<<"Root\n";
-        cout<<"  |- Block 0: Core (Tree Structure)\n";
-        cout<<"      |- Block 1: Provides PATH (Path handling)\n";
-        cout<<"          |- Block 1: Core (Add element by PATH)\n";
-        cout<<"              |- Block 2: Core (Print with indents)\n";
-        cout<<"                  |- Block 3: Core (Delete subtree by PATH)\n\n";
+
+    void print_tree() {
+        if (!root) cout << "Tree is empty\n";
+        else print_recursive(root, 0);
+    }
+
+    void print_bfs() {
+        if (!root) return;
+        queue<TreeNode*> q;
+        q.push(root);
+        while (!q.empty()) {
+            int sz = q.size();
+            for (int i = 0; i < sz; i++) {
+                TreeNode* curr = q.front(); q.pop();
+                cout << curr->data << " ";
+                for (auto c : curr->children) q.push(c);
+            }
+            cout << "\n";
+        }
+    }
+
+    void demo() {
+        cout << "--- N-ary Tree Demonstration ---\n";
+        add_node({}, 10);
+        add_node({}, 20);
+        add_node({}, 30);
+        add_node({0}, 40);
+        add_node({1}, 50);
+        
+        cout << "Added nodes (print with indents):\n";
+        print_tree();
+
+        cout << "\nLevel-order print (BFS bonus):\n";
+        print_bfs();
+
+        vector<ll> p;
+        if (get_path_to_element(50, p)) {
+            cout << "\nFound path to 50: ";
+            for (ll x : p) cout << x << " ";
+            cout << "\n";
+        }
+
+        cout << "\nDeleting subtree with value 20...\n";
+        TreeNode* del = delete_by_value(20);
+        clear_memory(del);
+        print_tree();
     }
 };
 
-int main(){
-    cout<<"Select Mode:\n1.Interactive\n2.Demo\n3.Task Tree (Bonus)\n";
-    ll mode;
-    cin>>mode;
-    NaryTree t;
-    if(mode==2){
-        t.demo();
-    }else if(mode==3){
-        t.print_task_tree();
-    }else if(mode==1){
-        while(true){
-            cout<<"Tree: 1.Add 2.Del 3.Print 0.Exit: ";
-            ll op;
-            cin>>op;
-            if(op==0)break;
-            if(op==1){
-                ll val;
-                cout<<"val: ";cin>>val;
-                cout<<"path length (0 for root/parent): ";
-                ll len;cin>>len;
-                vector<ll> p;
-                if(len>0){
-                    cout<<"path indices separated by space: ";
-                    for(ll i=0;i<len;i++){
-                        ll x;cin>>x;p.push_back(x);
+class BSTNode {
+public:
+    ll data;
+    BSTNode *left, *right;
+    BSTNode(ll v) : data(v), left(nullptr), right(nullptr) {}
+};
+
+class BinaryTree {
+    BSTNode* root;
+
+    BSTNode* insert_rec(BSTNode* node, ll val) {
+        if (!node) return new BSTNode(val);
+        if (val < node->data) node->left = insert_rec(node->left, val);
+        else node->right = insert_rec(node->right, val);
+        return node;
+    }
+
+    void pre_order(BSTNode* node, vector<ll>& res) {
+        if (!node) return;
+        res.push_back(node->data);
+        pre_order(node->left, res);
+        pre_order(node->right, res);
+    }
+
+public:
+    BinaryTree() { root = nullptr; }
+    void add(ll val) { root = insert_rec(root, val); }
+    
+    void demo_block_5() {
+        add(50); add(30); add(70); add(20); add(40);
+        vector<ll> seq;
+        pre_order(root, seq);
+        cout << "Pre-order (sequential) representation of BST: ";
+        for (ll x : seq) cout << x << " ";
+        cout << "\n";
+    }
+};
+
+class ExprNode {
+public:
+    string val;
+    ExprNode *left, *right;
+    ExprNode(string v) : val(v), left(nullptr), right(nullptr) {}
+};
+
+class ExprTree {
+    double eval(ExprNode* node) {
+        if (!node->left && !node->right) return stod(node->val);
+        double l = eval(node->left);
+        double r = eval(node->right);
+        if (node->val == "+") return l + r;
+        if (node->val == "*") return l * r;
+        return 0;
+    }
+public:
+    void demo() {
+        ExprNode* root = new ExprNode("*");
+        root->left = new ExprNode("+");
+        root->right = new ExprNode("4");
+        root->left->left = new ExprNode("2");
+        root->left->right = new ExprNode("3");
+        cout << "Expression: (2 + 3) * 4 = " << eval(root) << "\n";
+    }
+};
+
+int main() {
+    srand(time(0));
+    NaryTree tree;
+    BinaryTree bst;
+    ExprTree expr;
+
+    while (true) {
+        cout << "\n1.Interactive 2.Demo(Block 0-3) 3.Binary(Block 4-5) 4.Expressions(Block 6) 0.Exit\nChoose: ";
+        int mode; cin >> mode;
+        if (mode == 0) break;
+
+        if (mode == 2) {
+            tree.demo();
+        } else if (mode == 3) {
+            bst.demo_block_5();
+        } else if (mode == 4) {
+            expr.demo();
+        } else if (mode == 1) {
+            while (true) {
+                cout << "\n1.Add by path 2.Add with prob. 3.Print indents 4.Print levels 5.Find path 6.Delete by value 0.Back: ";
+                int op; cin >> op;
+                if (op == 0) break;
+
+                if (op == 1) {
+                    ll val, len;
+                    cout << "Value: "; cin >> val;
+                    cout << "Path length (0 for root): "; cin >> len;
+                    vector<ll> path;
+                    for (int i = 0; i < len; i++) {
+                        ll x; cin >> x; path.push_back(x);
                     }
+                    tree.add_node(path, val);
                 }
-                t.add_node(p,val);
-            }
-            if(op==2){
-                cout<<"path length: ";
-                ll len;cin>>len;
-                vector<ll> p;
-                if(len>0){
-                    cout<<"path indices separated by space: ";
-                    for(ll i=0;i<len;i++){
-                        ll x;cin>>x;p.push_back(x);
-                    }
+                else if (op == 2) {
+                    ll val; double p;
+                    cout << "Value: "; cin >> val;
+                    cout << "Probability (0.0 - 1.0): "; cin >> p;
+                    tree.add_with_probability(val, p);
                 }
-                TreeNode* del=t.delete_node(p);
-                if(del)cout<<"Deleted subtree with root: "<<del->date<<"\n";
-            }
-            if(op==3){
-                t.print_tree();
+                else if (op == 3) tree.print_tree();
+                else if (op == 4) tree.print_bfs();
+                else if (op == 5) {
+                    ll val;
+                    cout << "Search value: "; cin >> val;
+                    vector<ll> path;
+                    if (tree.get_path_to_element(val, path)) {
+                        cout << "Path: ";
+                        for (ll x : path) cout << x << " ";
+                        cout << "\n";
+                    } else cout << "Not found.\n";
+                }
+                else if (op == 6) {
+                    ll val;
+                    cout << "Delete value: "; cin >> val;
+                    TreeNode* del = tree.delete_by_value(val);
+                    if (del) {
+                        cout << "Deleted!\n";
+                        tree.clear_memory(del);
+                    } else cout << "Element not found.\n";
+                }
             }
         }
     }
